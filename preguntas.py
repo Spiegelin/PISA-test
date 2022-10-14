e0d1f3d9071dfdb593834cf482e6259b022191f6
Spiegelin <100853421+Spiegelin@users.noreply.github.com>
2022-10-13 22:26:26 -0500

    Update preguntas.py

--- preguntas.py
+++ preguntas.py
@@ -3,15 +3,15 @@ from random import sample
 
 # Lista donde dentro hay una lista de links de fotos y preguntas
 preguntas_examen_PISA = {
-    1 : ["/Users/spiegel/Documents/TEC/1 SEMESTRE/PENSAMIENTO COMPUTACIONAL/CODE/Proyecto Bloque/Preguntas/Pregunta 1/Screen Shot 2022-10-11 at 10.30.27.png", "/Users/spiegel/Documents/TEC/1 SEMESTRE/PENSAMIENTO COMPUTACIONAL/CODE/Proyecto Bloque/Preguntas/Pregunta 1/pregunta.txt"],
-    2 : ["/Users/spiegel/Documents/TEC/1 SEMESTRE/PENSAMIENTO COMPUTACIONAL/CODE/Proyecto Bloque/Preguntas/Pregunta 2/Copia de Screen Shot 2022-10-11 at 10.30.27.png", "/Users/spiegel/Documents/TEC/1 SEMESTRE/PENSAMIENTO COMPUTACIONAL/CODE/Proyecto Bloque/Preguntas/Pregunta 2/pregunta.txt"],
-    3 : ["/Users/spiegel/Documents/TEC/1 SEMESTRE/PENSAMIENTO COMPUTACIONAL/CODE/Proyecto Bloque/Preguntas/Pregunta 3/Screen Shot 2022-10-11 at 17.58.57.png", "/Users/spiegel/Documents/TEC/1 SEMESTRE/PENSAMIENTO COMPUTACIONAL/CODE/Proyecto Bloque/Preguntas/Pregunta 3/pregunta.txt"],
-    4 : ["", "/Users/spiegel/Documents/TEC/1 SEMESTRE/PENSAMIENTO COMPUTACIONAL/CODE/Proyecto Bloque/Preguntas/Pregunta 4/pregunta.txt"],
-    5 : ["/Users/spiegel/Documents/TEC/1 SEMESTRE/PENSAMIENTO COMPUTACIONAL/CODE/Proyecto Bloque/Preguntas/Pregunta 5/Screen Shot 2022-10-11 at 18.03.33.png", "/Users/spiegel/Documents/TEC/1 SEMESTRE/PENSAMIENTO COMPUTACIONAL/CODE/Proyecto Bloque/Preguntas/Pregunta 5/pregunta.txt"],
-    6 : ["/Users/spiegel/Documents/TEC/1 SEMESTRE/PENSAMIENTO COMPUTACIONAL/CODE/Proyecto Bloque/Preguntas/Pregunta 6/Screen Shot 2022-10-13 at 20.49.25.png", "/Users/spiegel/Documents/TEC/1 SEMESTRE/PENSAMIENTO COMPUTACIONAL/CODE/Proyecto Bloque/Preguntas/Pregunta 6/pregunta.txt"],
-    7 : ["/Users/spiegel/Documents/TEC/1 SEMESTRE/PENSAMIENTO COMPUTACIONAL/CODE/Proyecto Bloque/Preguntas/Pregunta 8/Screen Shot 2022-10-11 at 18.29.55.png", "/Users/spiegel/Documents/TEC/1 SEMESTRE/PENSAMIENTO COMPUTACIONAL/CODE/Proyecto Bloque/Preguntas/Pregunta 8/pregunta.txt"],
-    8 : ["/Users/spiegel/Documents/TEC/1 SEMESTRE/PENSAMIENTO COMPUTACIONAL/CODE/Proyecto Bloque/Preguntas/Pregunta 9/Screen Shot 2022-10-11 at 18.31.29.png", "/Users/spiegel/Documents/TEC/1 SEMESTRE/PENSAMIENTO COMPUTACIONAL/CODE/Proyecto Bloque/Preguntas/Pregunta 9/pregunta.txt"],
-    9 : ["/Users/spiegel/Documents/TEC/1 SEMESTRE/PENSAMIENTO COMPUTACIONAL/CODE/Proyecto Bloque/Preguntas/Pregunta 10/Screen Shot 2022-10-11 at 18.33.51.png", "/Users/spiegel/Documents/TEC/1 SEMESTRE/PENSAMIENTO COMPUTACIONAL/CODE/Proyecto Bloque/Preguntas/Pregunta 10/pregunta.txt"]
+    1 : ["./Pregunta 1/Screen Shot 2022-10-11 at 10.30.27.png", "./Pregunta 1/pregunta.txt"],
+    2 : ["./Pregunta 2/Copia de Screen Shot 2022-10-11 at 10.30.27.png", "./Pregunta 2/pregunta.txt"],
+    3 : ["./Pregunta 3/Screen Shot 2022-10-11 at 17.58.57.png", "./Pregunta 3/pregunta.txt"],
+    4 : ["", "./Pregunta 4/pregunta.txt"],
+    5 : ["./Pregunta 5/Screen Shot 2022-10-11 at 18.03.33.png", "./Pregunta 5/pregunta.txt"],
+    6 : ["./Pregunta 6/Screen Shot 2022-10-13 at 20.49.25.png", "./Pregunta 6/pregunta.txt"],
+    7 : ["./Pregunta 8/Screen Shot 2022-10-11 at 18.29.55.png", "./Pregunta 8/pregunta.txt"],
+    8 : ["./Pregunta 9/Screen Shot 2022-10-11 at 18.31.29.png", "./Pregunta 9/pregunta.txt"],
+    9 : ["./Pregunta 10/Screen Shot 2022-10-11 at 18.33.51.png", "./Pregunta 10/pregunta.txt"]
 }
 
 # Lista de keys del diccionario en orden aleatorio sin repetir
